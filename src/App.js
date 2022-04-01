3bcd48361a18d67d45150868a876cd4e26854ff1
Mike Gonzalez <gonzo@Mikes-MacBook-Air-2.local>
2022-03-31 18:24:54 -0700

    NFTicket Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Sport from './components/Sport'
+import Calltoaction from './components/Calltoaction'
+import { useEffect, useState } from 'react'
 
 function App() {
+      
+
+const [games, setGames] = useState ([])
+// const [time, setTime] = useState ([])
+
+useEffect (() => { 
+
+fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=VipiE7zQH6UYqMkh5ZpGSZP2U4M1gOrm')
+.then(response => (response.json()))
+.then((json) => {setGames(json._embedded.events)
+})
+
+}, [])
+
+// console.log(games)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">   
+       <About/>  
+        <Calltoaction/> 
+       <Sport games = {games} />
+       <Navbar/> 
     </div>
   );
 }
---
+++ src/components/Calltoaction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Calltoaction() {
+  return (
+
+    <div className='calltoaction'>
+        
+        <div className='calltoaction-info'>
+            <h1> Register Today </h1>
+            <h4> Get NFTickets delivered to your wallet and share them instantly with friends. Simple as that. </h4>
+            <button id = 'button'> Sign Up</button>
+        </div>
+    </div>
+    
+  )
+}
+
